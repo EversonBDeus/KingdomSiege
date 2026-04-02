4cdbdd0f1f058c6df87e6a6859a0c1538b53e284
EversonBDeus <everson.deus@outlook.com.br>
2026-04-02 18:09:56 -0400

    Cria estrutura inicial do projeto Fabric Kingdom Siege

---
+++ src/client/java/com/eversonbdeus/kingdomsiege/client/KingdomSiegeDataGenerator.java
@@ -0,0 +1,11 @@
+package com.eversonbdeus.kingdomsiege.client;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class KingdomSiegeDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
