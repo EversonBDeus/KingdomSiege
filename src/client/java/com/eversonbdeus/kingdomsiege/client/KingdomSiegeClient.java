31df96b46a38740d9459a52439b8355ee1d1c2da
EversonBDeus <everson.deus@outlook.com.br>
2026-04-02 18:16:35 -0400

    Limpa template inicial e define base do mod Kingdom Siege

--- src/client/java/com/eversonbdeus/kingdomsiege/client/KingdomSiegeClient.java
+++ src/client/java/com/eversonbdeus/kingdomsiege/client/KingdomSiegeClient.java
@@ -1,10 +1,11 @@
 package com.eversonbdeus.kingdomsiege.client;
 
+import com.eversonbdeus.kingdomsiege.KingdomSiege;
 import net.fabricmc.api.ClientModInitializer;
 
 public class KingdomSiegeClient implements ClientModInitializer {
 	@Override
 	public void onInitializeClient() {
-		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+		KingdomSiege.LOGGER.info("Inicializando cliente de {}.", KingdomSiege.MOD_NAME);
 	}
 }
--- src/client/java/com/eversonbdeus/kingdomsiege/client/KingdomSiegeDataGenerator.java
+++ src/client/java/com/eversonbdeus/kingdomsiege/client/KingdomSiegeDataGenerator.java
@@ -1,11 +1,13 @@
 package com.eversonbdeus.kingdomsiege.client;
 
+import com.eversonbdeus.kingdomsiege.KingdomSiege;
 import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
 
 public class KingdomSiegeDataGenerator implements DataGeneratorEntrypoint {
 	@Override
 	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
-
+		fabricDataGenerator.createPack();
+		KingdomSiege.LOGGER.info("Configurando gerador de dados de {}.", KingdomSiege.MOD_NAME);
 	}
 }
