8dfbe2ea7cdc2b25f76f2504639867f332ceb8ab
EversonBDeus <everson.deus@outlook.com.br>
2026-04-02 20:32:33 -0400

    Padroniza metadados e fecha a etapa 0 do projeto

--- src/main/java/com/eversonbdeus/kingdomsiege/KingdomSiege.java
+++ src/main/java/com/eversonbdeus/kingdomsiege/KingdomSiege.java
@@ -5,7 +5,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class KingdomSiege implements ModInitializer {
-	public static final String MOD_ID = "kingdom-siege";
+	public static final String MOD_ID = "kingdomsiege";
 	public static final String MOD_NAME = "Kingdom Siege";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
